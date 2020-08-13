912d0a65952cae57caa0ea9c90d75678f72a4609
TheRealTimeGuo <69281025+TheRealTimeGuo@users.noreply.github.com>
2020-08-13 08:28:40 +0800

    Add files via upload

---
+++ Aim2/Association score/CoassociationAlgorithm.py
@@ -0,0 +1,15 @@
+def score(pValue, Array):
+    Distances = []
+    Association_score = []
+    for value in Array:
+        Distances.append(1/abs(value-pValue))
+    Sum = sum(Distances)
+    for dist in Distances:
+        Association_score.append(dist/Sum)
+    return Association_score
+
+#print(Association_between(1,[2,2,4]))
+#print(Association_between(8.06458e-02,[1.64869e-01,8.64203e-01,5.81415e-02]))
+
+
+
