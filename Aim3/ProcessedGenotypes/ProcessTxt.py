1f50102fbe5812b3085371b4948952b6c602e959
RyanTooOp <42991620+RyanTooOp@users.noreply.github.com>
2020-08-16 16:27:07 +0800

    TxtCode Version1

--- Aim3/ProcessedGenotypes/ProcessTxt.py
+++ Aim3/ProcessedGenotypes/ProcessTxt.py
@@ -6,25 +6,67 @@ Created on Sun Aug 16 15:46:37 2020
 @author: ryanwang
 """
 
-totalFiles = ['BreastCancer_Co-association_score.txt',
-              'ChronicKidneyDisease_Co-association_score.txt',
-              'ChronicObstructivePulmonaryDisease_Co-association_score.txt',
-              'CoronaryArteryDisease_Co-association_score.txt',
-              'LungCancer_Co-association_score.txt',
-              'Obesity_Co-association_score.txt',
-              'Type2Diabetes_Co-association_score.txt']
+currentFile = "hu8A5FBF" + ".txt"
 
 def run():
-#    out1 = open("CombinedHap.txt", "w")
-    in1 = open("Data/hu8A5FBF.txt", "r")
+    out1 = open("GenotypeMatrix.txt", "w")
+    addIndividual(currentFile, out1)
+    
+    out1.close()
+
+def addIndividual(currentFile, out1):
+    in1 = open("Data/" + currentFile, "r")
+    hapFile = open("CombinedHap.txt")
     tempLine = in1.readline().strip().split("\t")
+    tempHap = hapFile.readline().strip().split("\t")
  #   tempLine = repr(in1.readline())
     while(tempLine[0] != "# rsid"):
 #    for i in range(15):
 #        print(tempLine)
         tempLine = in1.readline().strip().split("\t")
- #       tempLine = repr(in1.readline())
+#        tempLine = repr(in1.readline())
+    tempLine = in1.readline().strip().split("\t")
     print(tempLine)
-#    out1.close()
-
+    hapChrom = int(tempHap[0])
+    hapPosStart = int(tempHap[1])
+    hapPosEnd = int(tempHap[2])
+    hapArr = []
+    hapArr += ["0"]
+    hapIndex = 0
+    
+    
+    tempLine = in1.readline().strip().split("\t")
+    currChrom = int(tempLine[1])
+    currPos = int(tempLine[2])
+    
+    isHap = False#False = tempLine incrased, while True = hap increased
+    while ((tempLine != [""]) and (tempHap != [""])):
+        if isHap:
+            hapChrom = int(tempHap[0])
+            hapPosStart = int(tempHap[1])
+            hapPosEnd = int(tempHap[2])
+            hapIndex += 1
+            hapArr += ["0"]
+        else:
+            currChrom = int(tempLine[1])
+            currPos = int(tempLine[2])
+        if currChrom < hapChrom:
+            tempLine = in1.readline().strip().split("\t")
+            isHap = False
+        elif currChrom > hapChrom:
+            tempHap = hapFile.readline().strip().split("\t")
+            isHap = True
+        else:
+            if currPos < hapPosStart:
+                tempLine = in1.readline().strip().split("\t")
+                isHap = False
+            elif currPos > hapPosStart and currPos < hapPosEnd:
+                hapArr[hapIndex] = "1"
+                tempLine = in1.readline().strip().split("\t")
+                isHap = False
+            else:
+                tempHap = hapFile.readline().strip().split("\t")
+                isHap = True
+    returnStr = "\t".join(hapArr)
+    out1.write(returnStr + "\n")
 run()
